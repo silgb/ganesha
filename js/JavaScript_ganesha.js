a0bcef9b37d5b39efb6631b3426f72d000097c6f
silgb <sil.boglietti@gmail.com>
2022-10-11 01:56:41 -0300

    primer commit

---
+++ js/JavaScript_ganesha.js
@@ -0,0 +1,22 @@
+// COMIENZO de boton de subir
+
+const btn_scrolltop = document.getElementById("btn_scrolltop")
+      btn_scrolltop.addEventListener('click', () => {
+        window.scrollTo(0, 0)
+      })
+
+    window.onscroll = () => {
+      add_btn_scrolltop()
+    }
+
+    const add_btn_scrolltop = () => {
+      if (window.scrollY < 300) {
+        btn_scrolltop.classList.remove("btn-scrolltop-on")
+      } else {
+        btn_scrolltop.classList.add("btn-scrolltop-on")
+      }
+    }
+
+
+
+// FIN de boton de subir
